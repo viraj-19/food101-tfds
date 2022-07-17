bbac561244294ae837cdf65775bc68f98aa32d13
Viraj Jadhav <virajhbk.1010@gmail.com>
2022-07-17 20:33:44 +0530

    changes

--- app11.py
+++ app11.py
@@ -50,7 +50,7 @@ else:
 
 	pred = predict_class(np.asarray(test_image), model)
 
-	class_names = ['baby_back_ribs','baklava','beef_carpaccio','beef_tartare','beet_salad','beignets','bibimbap','bread_pudding','breakfast_burrito','bruschetta','caesar_salad',
+	class_names = ['apple_pie','baby_back_ribs','baklava','beef_carpaccio','beef_tartare','beet_salad','beignets','bibimbap','bread_pudding','breakfast_burrito','bruschetta','caesar_salad',
 	'cannoli','caprese_salad','carrot_cake','ceviche','cheesecake','cheese_plate','chicken_curry','chicken_quesadilla','chicken_wings','chocolate_cake','chocolate_mousse','churros',
     'clam_chowder','club_sandwich','crab_cakes','creme_brulee','croque_madame','cup_cakes','deviled_eggs','donuts','dumplings','edamame','eggs_benedict','escargots','falafel',
     'filet_mignon','fish_and_chips','foie_gras','french_fries','french_onion_soup','french_toast','fried_calamari','fried_rice','frozen_yogurt','garlic_bread','gnocchi',
@@ -60,9 +60,9 @@ else:
     'scallops','seaweed_salad','shrimp_and_grits','spaghetti_bolognese','spaghetti_carbonara','spring_rolls','steak','strawberry_shortcake','sushi','tacos','takoyaki','tiramisu',
     'tuna_tartare','waffles']
 
-	result = class_names[np.argmax(pred)]
+	#result = class_names[np.argmax(pred)]
 
-	output = 'The image is a ' + result
+	output = 'The image is a ' + str(pred)#result
 
 	slot.text('Done')
 
